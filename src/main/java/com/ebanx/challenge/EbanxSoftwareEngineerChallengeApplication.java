f52651318a1e872fc1228a202958493ed7db65e9
Gabriel Santos <gabrielgeraldo297@gmail.com>
2026-01-18 19:28:48 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/ebanx/challenge/EbanxSoftwareEngineerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ebanx.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanxSoftwareEngineerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbanxSoftwareEngineerChallengeApplication.class, args);
+	}
+
+}
